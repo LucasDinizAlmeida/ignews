9e6889098ee3b89952cefbe016c54688892f7e9b
Lucas Diniz Almeida <almeidalucas820@gmail.com>
2022-02-15 09:15:44 -0300

    :tada: first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss'
+
+
+export function Header() {
+
+  return(
+    <header className={styles.Component} >
+      <div className={styles.Content}>
+        <img src="/aqui/images/logo.svg" alt="ignews" />
+        <nav>
+          <a className={styles.isActive}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
