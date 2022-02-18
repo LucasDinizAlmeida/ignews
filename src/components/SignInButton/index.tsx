6c94f2e064fd8c9515f5724f2ef20ad0b35397c4
Lucas Diniz Almeida <almeidalucas820@gmail.com>
2022-02-18 08:36:19 -0300

    :tada: using next-auth as a form of authentication

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,31 @@
+
+import { useSession, signIn, signOut } from 'next-auth/react';
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
 
+
 export function SignInButton() {
 
-  const isUserLoggedIn = true
+  const { data: session } = useSession();
 
-  return isUserLoggedIn? (
+  console.log(session)
+
+  return session? (
     <button 
       className={styles.signInButton}
       type='button'
+      onClick={() => signOut()}
     >
       <FaGithub color='#84d361'/>
-      Lucas Diniz Almeida
+      {session.user.name}
       <FiX color='#737380' />
     </button>
   ) : (
     <button 
       className={styles.signInButton}
       type='button'
+      onClick={() => signIn('github')}
     >
       <FaGithub color='#eba417'/>
       Sign in with GitHub
