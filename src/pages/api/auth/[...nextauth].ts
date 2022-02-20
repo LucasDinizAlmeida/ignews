1ccd770e8b2246db3e0b96d99091dc59cb418234
Lucas Diniz Almeida <almeidalucas820@gmail.com>
2022-02-20 12:39:06 -0300

    :tada: creating authentication through github

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { query as q } from "faunadb"
+import { fauna } from "../../../services/fauna"
+
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -8,4 +11,40 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+
+      const { email: userEmail } = user
+
+      try {
+        fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_userEmail'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { userEmail }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_userEmail'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+
+        return true
+      } catch  {
+        return false
+      }
+    }
+  }
 })
